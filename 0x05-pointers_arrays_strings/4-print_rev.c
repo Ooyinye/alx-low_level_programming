a660f0290a2d19ba60f004890f45266224f635e8
Ooyinye <okorieonyinyechi23@gmail.com>
2023-05-02 09:41:00 -0700

    print rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,20 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
+	int longi = 0;
+	int o;
 
-	while (*s)
+	while (*s != '\0')
 	{
+		longi++;
 		s++;
-		count++;
 	}
-	while (count)
+	s--;
+	for (o = longi; o > 0; o--)
 	{
-		s--;
 		_putchar(*s);
-		count--;
+		s--;
 	}
+
 	_putchar('\n');
 }
