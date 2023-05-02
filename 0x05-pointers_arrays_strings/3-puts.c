4d48b38c5edbd46eab78b53d49e587c7bde8e460
Ooyinye <okorieonyinyechi23@gmail.com>
2023-05-02 07:45:00 -0700

    puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new lineto stdout.
+ * @str: string to be printed
+ * Return: nothing on success
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
