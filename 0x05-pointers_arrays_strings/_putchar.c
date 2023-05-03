cb90b3331fd4060091ce0a56008b6ec8e3095267
Ooyinye <okorieonyinyechi23@gmail.com>
2023-05-02 22:38:31 -0700

    print_rev.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * _putchar - writes character to stdout 
+ * @c: char to be printed
+ * Return: on success 1
  */
 int _putchar(char c)
 {
