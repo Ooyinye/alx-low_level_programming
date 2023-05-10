17dd7a7bc827116128df642d1a0e48fbc2e323a6
Ooyinye <okorieonyinyechi23@gmail.com>
2023-05-10 05:30:09 -0700

    puts recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
-int _putchar.c(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -11,4 +10,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _putchar(char c);
 #endif
